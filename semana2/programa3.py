89c15f16862b00dd4a8b26be534ebbc433352724
osaradahir <122835599+osaradahir@users.noreply.github.com>
2023-01-24 15:00:32 +0000

    formatear

---
+++ semana2/programa3.py
@@ -0,0 +1,12 @@
+"""
+    Programa3
+    Nombre:Oscar Adahir GS.
+    Fecha:24/01/2023.
+    Descripcion:Formatear variables de str
+"""
+variable1=10 #  variable de tipo int.
+variable2=5 #  variable de tipo int.
+print(variable1+variable2) #imprime la suma de las dos variables.
+print("{}+{}={}".format(variable1,variable2,(variable1+variable2)))#  represemta la validacion de una suma.
+print("{}={}*{}".format(variable2,variable1,(variable2*variable1))) #  representa la multiplicacion de dos variables.
+print("El resultado de dividir {}/{}={}".format(variable1,variable2,(variable1/variable2)))# representa la division de dos variables.
