8a762db97f8e208abbca0995d254cdc469c5ba29
osaradahir <122835599+osaradahir@users.noreply.github.com>
2023-01-30 14:55:53 +0000

    pregunta un numero y lo combierte de str a int.

---
+++ semana3/programa4.py
@@ -0,0 +1,18 @@
+"""
+    Programa4
+    Nombre:Oscar Adahir GS.
+    Fecha:30/01/2023.
+    Descripcion:pregunta un numero y lo combierte de str a int.
+"""
+numero1 = int(input("Numero 1: "))# pregunta un numero
+numero2 = input("Numero 2: ")# variable entera
+suma= numero1+int(numero2)# suma de un numero
+print (suma)# imprecion del resultado de la suma
+resta= numero1 - int(numero2)# resta de un numero
+print(resta)# impresion del resultado 
+multiplicasion= numero1 * int(numero2)# multiplicasion de un numero
+print(multiplicasion)# impresion del resultado 
+division= numero1 / int(numero2)# divison de un numero
+print(division)# impresion del resultado 
+potencia= numero1** int(numero2)# potencia de un numero
+print(potencia)# impresion del resultado 
