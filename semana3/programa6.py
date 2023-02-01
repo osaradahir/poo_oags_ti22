05552646811f15de8e6931c7749c9374efb11eb9
Oscar_AGS <122835599+osaradahir@users.noreply.github.com>
2023-02-01 14:45:19 +0000

    condiciones, estructura de control.

---
+++ semana3/programa6.py
@@ -0,0 +1,12 @@
+"""
+    Programa5
+    Nombre:Oscar Adahir GS.
+    Fecha:1/02/2023.
+    Descripcion: condiciones, estructura de control.
+"""
+numero1= int(input("Numero1: "))# pregunta un numero
+numero2= int(input("Numero2: "))# pregunta un numero
+if numero1>=numero2:# condiciona un numero para encontrar el mayor
+     print(numero1)# imprime el numero1 mayor
+else:
+     print(numero2)# sino imprime el numero2 mayor 
