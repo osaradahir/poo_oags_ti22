bca17d77f44fa3af24f27894a581573d947c1573
Oscar_AGS <122835599+osaradahir@users.noreply.github.com>
2023-02-13 13:38:48 +0000

    1

---
+++ semana5/programa10.py
@@ -0,0 +1,14 @@
+"""
+    Programa10
+    Nombre:Oscar Adahir GS.
+    Fecha:13/02/2023.
+    Descripcion:clases, objetos, 
+"""
+class Persona:
+    nombres=None
+    def __int__(self):
+         print("Persona")
+
+objeto= Persona()
+objeto1= Persona()
+print(objeto.nombres)
